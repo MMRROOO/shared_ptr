ff86d32815bffbb699221968e29a9a898097ee61
Mario Mrowka <mrowkamaad@gmail.com>
2024-01-16 15:47:35 -0500

    working adding pointers

--- main.cpp
+++ main.cpp
@@ -1,44 +1,72 @@
+#include <iostream>
 
-template<typename P>
-class ControlBlock{
-
-    public:
-    ControlBlock(P* object_ = nullptr): 
-        object(object_), 
-        shared_pointers(1)
-    {}
-
-    P* get_object(){
-        return object;
-    }
-
-    void operator=(P* new_obj){  
-        object = new_obj;
-    }
-
-    private:
-    int shared_pointers;
-    P* object;
-};
 
 template<typename P>
 class sharedPtr{
 
     public:
-    sharedPtr():
-        control()
+    explicit sharedPtr(P* pointer_ = nullptr):
+       stored_pointer(pointer_),
+       count(new int(1))
     {}
 
-    void operator=(P* new_obj){
-        control = new_obj;
+    sharedPtr(const sharedPtr& pointer_):  
+    stored_pointer(pointer_.stored_pointer),
+    count(pointer_.count)
+    {
+        (*count)++;
+    }
+    
+
+    // bool operator!=(sharedPtr other){
+    //     return !(count == other.count && stored_pointer == other.stored_pointer);
+    // }
+
+    sharedPtr& operator=(sharedPtr& other){
+        if (this != &other){
+            count = other.count;
+            stored_pointer = other.stored_pointer;
+            (*count) ++;
+            
+        }
+        return *this;
     }
 
-    
+
+
     P* get(){
         return stored_pointer;
     }
 
+    int get_copies_number(){
+        return *count;
+    }
+
+
     private:
-    ControlBlock<P> control;
+
+    sharedPtr& copy(sharedPtr& shared_ptr){
+        
+    }
+
+
+    int* count;
     P* stored_pointer;
 };
+
+int main(){
+
+    int num = 2;
+    sharedPtr<int> a = sharedPtr(&num);
+    sharedPtr<int> b = sharedPtr(&num);
+
+    b = a;
+    std::cout << b.get_copies_number() << "\n";
+
+    sharedPtr<int> c = b;
+
+    
+    std::cout << b.get_copies_number() << "\n";
+    std::cout << a.get_copies_number() << "\n";
+
+}
